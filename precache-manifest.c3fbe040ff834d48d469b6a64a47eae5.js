545b82866c1ac768a5ffbd69f40fec839c81b2a8
Code Sandbox <hello@codesandbox.io>
2021-06-15 19:56:37 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c3fbe040ff834d48d469b6a64a47eae5.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d2e189c1cf1fcc658dba051b035685",
+    "url": "https://igiboi.github.io/csb-12otj/index.html"
+  },
+  {
+    "revision": "19c1e7e0c3a33232040e",
+    "url": "https://igiboi.github.io/csb-12otj/static/js/main.d2d94976.chunk.js"
+  },
+  {
+    "revision": "d521b2f151983c4b96ab",
+    "url": "https://igiboi.github.io/csb-12otj/static/js/runtime-main.f01d0e2f.js"
+  }
+]);
